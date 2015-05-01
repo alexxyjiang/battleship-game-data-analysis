26f12eadd8a36ae34981f8dd813521170e08ce19
Xueyuan Jiang <alexxyjiang@gmail.com>
2015-05-01 23:06:28 +0800

    fix pre commit

--- prepare_data.py
+++ prepare_data.py
@@ -1,17 +1,72 @@
-#!/usr/bin/python
-import sys
+#/usr/bin/python
+#-*- encoding: utf-8 -*-
+import ConfigParser
 
-# load ship conf, assign index
-def load_ship_conf(file_ship_conf):
-    handle  = open(file_ship_conf)
-    count   = 0
-    for line in handle:
-        dict_ship[line.strip('\n')] = count
-        count                       += 1
-    handle.close()
+dict_index  = {}
+dict_res    = {}
+dict_ships  = {}
+dict_name   = {}
+count       = 0
+
+# load all support resource
+def load_resource_types(list_resource):
+    global count 
+    for item in list_resource:
+        dict_index[count]   = item[1]
+        dict_res[item[1]]   = count
+        count               += 1
+
+# load all ship types 
+def load_ship_types(list_ship):
+    global count
+    for item in list_ship:
+        dict_index[count]   = item[1]
+        dict_ships[item[1]] = count
+        dict_name[item[0]]  = item[1]
+        count               += 1
+
+# process one file
+def process_one_file(handle_in, handle_out):
+    for line in handle_in:
+        items   = line.strip('\n').split('\t')
+        if len(items) != 6:
+            continue
+        list_out    = items[0:len(dict_res)]
+        ship_out    = items[-1]
+        ship_index  = dict_ships[dict_name[ship_out]]
+        for i in range(len(dict_res), len(dict_index)):
+            if i != ship_index:
+                list_out.append('0')
+            else:
+                list_out.append('1')
+        handle_out.write('\t'.join(list_out))
+        handle_out.write('\n')
+
+# do all
+def do_work(file_config):
+    confp   = ConfigParser.ConfigParser()
+    confp.read(file_config)
+    if confp.has_section('resource_types'):
+        load_resource_types(confp.items('resource_types'))
+    if confp.has_section('ship_types'):
+        load_ship_types(confp.items('ship_types'))
+    if confp.has_option('output', 'output_file'):
+        handle_out  = open(confp.get('output', 'output_file'), 'w')
+        list_title  = []
+        for index in range(len(dict_index)):
+            list_title.append(dict_index[index])
+        handle_out.write('\t'.join(list_title))
+        handle_out.write('\n')
+        if confp.has_section('input'):
+            for item in confp.items('input'):
+                if item[0].find('build_data') != -1:
+                    handle_in   = open(item[1])
+                    process_one_file(handle_in, handle_out)
+                    handle_in.close()
+        handle_out.close()
 
 def main():
-    pass
+    do_work('./conf/convert.conf')
 
 if __name__ == '__main__':
     main()
